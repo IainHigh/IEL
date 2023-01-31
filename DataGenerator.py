d7bd72691dc2122674cfc18ce579044bdce43252
Iain High <I.High@sms.ed.ac.uk>
2023-01-31 11:28:27 +0000

    Jesse, I forgot the git message Jesse!
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠜⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⠿⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿
    ⣿⣿⡏⠁⠀⠀⠀⠀⠀⣀⣠⣤⣤⣶⣶⣶⣶⣶⣦⣤⡄⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿
    ⣿⣿⣷⣄⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡧⠇⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣾⣮⣭⣿⡻⣽⣒⠀⣤⣜⣭⠐⢐⣒⠢⢰⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⡟⣾⣿⠂⢈⢿⣷⣞⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣷⣶⣾⡿⠿⣿⠗⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠻⠋⠉⠑⠀⠀⢘⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⣿⡿⠟⢹⣿⣿⡇⢀⣶⣶⠴⠶⠀⠀⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⣿⣿⣿⡿⠀⠀⢸⣿⣿⠀⠀⠣⠀⠀⠀⠀⠀⡟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
    ⣿⣿⣿⡿⠟⠋⠀⠀⠀⠀⠹⣿⣧⣀⠀⠀⠀⠀⡀⣴⠁⢘⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿
    ⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⠗⠂⠄⠀⣴⡟⠀⠀⡃⠀⠉⠉⠟⡿⣿⣿⣿⣿
    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠾⠛⠂⢹⠀⠀⠀⢡⠀⠀⠀⠀⠀⠙⠛⠿⢿
    
    Who are you talking to right now? Who is it that you think you see?
    Do you know how much commits I make a year? I mean, even if I tell you,
    you wouldn't believe it. Do you know what happens if I suddenly
    decide to stop coding? A buisness big enough it could be listed
    on the NASDAQ goes belly-up. Gone. It ceases to exist without me. No,
    you clearly don't know who you're talking to so let me clue you in.
    I am not commiting untested code, Skyler. I am the untested code.
    A guy pushes his code to review and gets rejected, and you think that of me?
    No, I am the one who reviews.

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,16 +1,142 @@
+from enum import Enum
+from urllib.request import urlopen
+import json
+from DataGeneratorPredictors import Predictors
+
+
 class DataGenerator():
     
-    def __init__():
+    def __init__(self,
+                numOfSamples : int = 100,
+                rainfall : list() = None, 
+                startingWaterLevel : float = 1.0,
+                volumeOfWaterComingFromDamn: list() = None,
+                catchementArea: float = 1.0,
+                pollutants: list() = None,
+                property_flooding_level: float = 1.0,
+                low_lying_land_flooding_level: float = 1.0):
+        
+        # Volume of water coming from damn is in m3/s
+        # Flow rate is in m3/s
+        # Rainfall is in mm
+        # Catchement area is in km2
+        # Water level is in m
+
+        # If the volume of water coming from the damn is not specified, set it to 0
+        if volumeOfWaterComingFromDamn is None:
+            volumeOfWaterComingFromDamn = [0.0] * numOfSamples
+
+        self.predictors = Predictors()
+
+        self.numOfSamples = numOfSamples # Number of quarter hourly samples we should generate
+        
+        if rainfall is None:
+            self.rainfall = self.read_rainfall_from_SEPA_api(samples = numOfSamples)
+        else:
+            self.rainfall = rainfall
+
+        self.startingWaterLevel = startingWaterLevel                    # The starting water level of the river
+        self.volumeOfWaterComingFromDamn = volumeOfWaterComingFromDamn  # The volume of water coming from the damn
+        self.catchementArea = catchementArea                            # The area of the catchement
+        self.pollutants = pollutants                                    # The pollutants in the river
+        self.property_flooding_level = property_flooding_level              # The level at which property flooding occurs
+        self.low_lying_land_flooding_level = low_lying_land_flooding_level  # The level at which low lying land flooding occurs (e.g. farm, marshland)
+
+    def calculate_quarter_hourly_flow_rate(self, quarterHourlyLevels = None):
+        curWaterLevel = self.startingWaterLevel
+        flowRate = [0.0]*self.numOfSamples
+
+
+        for i in self.numOfSamples:
+            flowRate[i] = self.predictors.generateQuarterHourlyFlow(curWaterLevel)
+
+            if quarterHourlyLevels is not None:
+                curWaterLevel = quarterHourlyLevels[i]
+
+        return flowRate
+
+    def calculate_quarter_hourly_water_diff(self, flowRate):
+        # Convert the mm of rainfall to m3/s
+        # 1mm of precipitation per 15 minutes per km2 = 0.9m3/s * catchmentarea
+        # However, only 72% of the water is actually available to the river
+        rainfallVol = [x * self.catchementArea * 0.9 * 0.72 for x in self.rainfall]
+        return [x + y - z for x, y, z in zip(self.volumeOfWaterComingFromDamn, rainfallVol, flowRate)]
+
+    def calculateDailyWaterDifference(self, qtrHourlySamples):
+        return sum(qtrHourlySamples)
+
+    def calculateDailyLevelDerivative(self, dailyWaterDifference):
+        return self.predictors.generateLevelDerivativeFromWaterDifference(dailyWaterDifference)
+
+    def calculateQuarterHourlyLevelDerivative(self, dailyLevelDerivative, qtrHourlyWaterDifference):
+        totalWaterDifference = sum(qtrHourlyWaterDifference)
+        quarterHourlyLevel = [0.0]*len(qtrHourlyWaterDifference)
+        for i in range(len(qtrHourlyWaterDifference)):
+            quarterHourlyLevel[i] = qtrHourlyWaterDifference[i] / totalWaterDifference
+        return quarterHourlyLevel
+
+    def calculateQuarterHourlyLevel(self, quarterHourlyLevelDerivative, startingWaterLevel):
+        level = [0.0]*len(quarterHourlyLevelDerivative)
+        level[0] = startingWaterLevel
+        for i in range(1, len(quarterHourlyLevelDerivative)):
+            level[i] = level[i-1] + quarterHourlyLevelDerivative[i]
+        return level
+
+    def calculateWaterDifference(self, flowRate, rainfall, volumeOfWaterComingFromDamn):
+        return (volumeOfWaterComingFromDamn + sum(rainfall)) - sum(flowRate)
+
+    def write_to_csv(timeframe : str = "qtr_hr", filename : str = "data.csv"):
+        # Timeframe:
+        #   qtr_hr = quarter hourly
+        #   hr = hourly
+        #   day = daily
+
         pass
 
-    def write_to_csv():
-        pass
+    def read_rainfall_from_SEPA_api(self, stationName : str = "Dippen", samples = 100):
+        url = "https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0&request=getTimeseriesList&station_name=" + stationName + "&format=json"
+  
+        # store the response of URL
+        response = urlopen(url)
+  
+        # storing the JSON response 
+        # from url in data
+        data_json = json.loads(response.read())
+  
+        # Get the id of the entry with "15minute.Total" and "Precip"
+        for entry in data_json:
+            if entry[4] == "15minute.Total" and entry[6] == "Precip":
+                id = entry[3]
+                break
 
-    def generate_rainfall():
-        pass
+        period = "P" + str((samples // 4) + 4) + "H"
+        url = "https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0&request=getTimeseriesValues&ts_id=" + str(id) + "&period=" + period + "&returnfields=Timestamp,%20Value,%20Quality%20Code&format=json"
+        
+        response = urlopen(url)
+        data_json = json.loads(response.read())
+
+        return [x[1] for x in data_json[0]["data"]][0:samples]
 
     def calculate_water_level(current_water_level, rainfall, flow_rate):
         pass
 
     def calculate_flow_rate(current_water_level):
         pass
+
+class eventEnum(Enum):
+    STORM = 1
+    DROUGHT = 2
+    FREEZE = 3
+    FLOOD = 4
+    BURST_DAM = 5
+
+if __name__ == "__main__":
+    # 96 samples = 24 hours
+    dg = DataGenerator(numOfSamples=96)
+    quarter_hourly_levels = None
+
+    for i in range(5):
+        quarter_hourly_flow_rate = dg.calculate_quarter_hourly_flow_rate(quarter_hourly_levels)
+        quarter_hourly_water_difference = dg.calculate_quarter_hourly_water_diff(quarter_hourly_flow_rate)
+
+    
---
+++ DataGeneratorPredictors.py
@@ -0,0 +1,149 @@
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+class Predictors:
+
+    def __init__(self):
+        self.qtrData = self.setUpQuarterHourly()
+        self.dailyData = self.setUpDaily()
+
+        self.quarterHourlyFlowAgainstLevel()
+        self.dailyLevelAgainstWaterDifference()
+
+    def setUpQuarterHourly(self):
+        # Import the data
+        flow = pd.read_csv('Data/Quater_Hourly_Readings/Quarter_Hourly_Flow_Rate.csv', delimiter=';')
+        flow.rename(columns={'Value': 'Mean Flow (m3/s)'}, inplace=True)
+        rain = pd.read_csv('Data/Quater_Hourly_Readings/Quarter_Hourly_Precipitation.csv', delimiter=';')
+        rain.rename(columns={'Value': 'Precipitation (mm)'}, inplace=True)
+        level = pd.read_csv('Data/Quater_Hourly_Readings/Quarter_Hourly_Level.csv', delimiter=';')
+        level.rename(columns={'Value': 'Water Level (m)'}, inplace=True)
+
+        # Merge the data
+        merged = pd.merge(flow, rain, on='#Timestamp')
+        merged = pd.merge(merged, level, on='#Timestamp')
+        merged = merged.drop('Quality Code', axis=1)
+        merged = merged.drop('Quality Code_x', axis=1)
+        merged = merged.drop('Quality Code_y', axis=1)
+        merged.dropna(inplace=True)
+
+        return merged
+
+    def setUpDaily(self):
+        # Import the new data and create a dataframe
+        daily_flow = pd.read_csv('Data/Daily_Averages/Daily_Mean_Flow_Rate.csv', delimiter=';')
+        daily_flow.rename(columns={'Value': 'Mean Flow (m3/s)'}, inplace=True)
+        daily_rain = pd.read_csv('Data/Daily_Averages/Daily_Precipitation.csv', delimiter=';')
+        daily_rain.rename(columns={'Value': 'Daily Precipitation (mm)'}, inplace=True)
+        daily_level = pd.read_csv('Data/Daily_Averages/Daily_Mean_Level.csv', delimiter=';')
+        daily_level.rename(columns={'Value': 'Mean Water Level (m)'}, inplace=True)
+
+        # Merge the 3 datasets into one
+        merged = pd.merge(daily_flow, daily_rain, on=['#Timestamp'])
+        merged = pd.merge(merged, daily_level, on=['#Timestamp'])
+        merged = merged.drop('Quality Code_x', axis=1)
+        merged = merged.drop('Quality Code_y', axis=1)
+        merged = merged.drop('Quality Code', axis=1)
+        merged.dropna(inplace=True)
+        return merged
+
+    def quarterHourlyFlowAgainstLevel(self, plotGraph = False, displayStats = False):
+        X = self.qtrData['Water Level (m)'].values.reshape(-1, 1)
+        y = self.qtrData['Mean Flow (m3/s)'].values.reshape(-1, 1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+        quadratic = PolynomialFeatures(degree=4)
+        X_quad = quadratic.fit_transform(X)
+        X_quad_test = quadratic.fit_transform(X_test)
+        lr = LinearRegression()
+        lr.fit(X_quad, y)
+
+        # Calculate and display the mean squared error and R^2
+        if displayStats:
+            y_quad_fit = lr.predict(X_quad)
+            y_quad_pred = lr.predict(X_quad_test)
+            print('MSE train: %.3f, test: %.3f' % (
+                mean_squared_error(y, y_quad_fit),
+                mean_squared_error(y_test, y_quad_pred)))
+            print('R^2 train: %.3f, test: %.3f' % (
+                r2_score(y, y_quad_fit),
+                r2_score(y_test, y_quad_pred)))
+
+        # Plot the polynomial curve
+        if plotGraph:
+            x = np.linspace(0.1, 3.5, 1000)
+            y = lr.predict(quadratic.fit_transform(x.reshape(-1, 1)))
+            plt.scatter(X, y, label='data points', color='lightgray', marker='.')
+            plt.plot(x, y, label='predicted', color='red')
+            plt.legend(loc='upper left')
+            plt.title('Regression of Flow Rate against Water Level')
+            plt.xlabel('Water Level (m)')
+            plt.ylabel('Flow Rate (m3/s)')
+            plt.show()
+
+        self.lr1 = lr
+
+    def generateQuarterHourlyFlow(self, level):
+        quadratic = PolynomialFeatures(degree=4)
+        return self.lr1.predict(quadratic.fit_transform(level.reshape(-1, 1)))
+
+    def dailyLevelAgainstWaterDifference(self, plotGraph = False, displayStats = False):
+        # Remove days with water difference > 2000000 and > 2000000
+        typical = self.dailyData[self.dailyData['Water Difference (m3)'] < 2000000]
+        typical = typical[typical['Water Difference (m3)'] > -2000000]
+
+        # Fit a polynomial curve of Water difference (independent) and level derivative (dependent)
+        X = typical['Water Difference (m3)'].values.reshape(-1, 1)
+        y = typical['level_derivative'].values.reshape(-1, 1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+        quadratic = PolynomialFeatures(degree=1)
+        X_quad = quadratic.fit_transform(X)
+        X_quad_test = quadratic.fit_transform(X_test)
+        lr = LinearRegression()
+        lr.fit(X_quad, y)
+
+        # Calculate and display the mean squared error and R^2
+        y_quad_fit = lr.predict(X_quad)
+        if displayStats:
+            y_quad_pred = lr.predict(X_quad_test)
+            print('MSE train: %.3f, test: %.3f' % (
+                mean_squared_error(y, y_quad_fit),
+                mean_squared_error(y_test, y_quad_pred)))
+            print('R^2 train: %.3f, test: %.3f' % (
+                r2_score(y, y_quad_fit),
+                r2_score(y_test, y_quad_pred)))
+
+        if plotGraph:
+            # Plot the polynomial curve
+            plt.scatter(X, y, label='data points', color='lightgray', marker='.')
+
+            x = np.linspace(min(X), max(X), 1000)
+            y = lr.predict(quadratic.fit_transform(x.reshape(-1, 1)))
+            plt.plot(x, y, label='predicted', color='red')
+
+            plt.xlim(-2000000, 2000000)
+            plt.ylim(-1, 2)
+
+            plt.legend(loc='upper left')
+            plt.title('Regression of Flow Rate against Water Level')
+            plt.xlabel('Water Difference (m3)')
+            plt.ylabel('Level Derivative (m)')
+            plt.show()
+
+        # Calculate the standard deviation of the residuals
+        residuals = y_quad_pred - y_test
+        
+        # Return the linear regression model, the residuals and the X values
+        self.lr2 = lr
+        self.residuals2 = residuals
+        
+    def generateLevelDerivativeFromWaterDifference(self, waterDifference):
+        quadratic = PolynomialFeatures(degree=1)
+
+        temp = self.lr2.predict(quadratic.fit_transform(waterDifference.reshape(-1, 1)))
+        return random.gauss(temp, np.std(self.residuals2))
